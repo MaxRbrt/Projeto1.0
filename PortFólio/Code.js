13cd5ed0755bc13a2cc1cb9e7e2948db53eafacc
MaxRbrt <Maxrt3765@gmail.com>
2025-05-26 10:44:36 -0300

    Adicionando efeito em JS, Simulando Json

--- PortFólio/Code.js
+++ PortFólio/Code.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const nav = document.getElementById('nav');
 
     menu.addEventListener('click', function () {
-        // Alterna a visibilidade do menu de navegação
+        
         nav.classList.toggle('active');
 
-        // Animação do botão hamburguer
+        
         menu.classList.toggle('open');
 
-        // Animação das barras
+        
         const bar1 = menu.querySelector('.bar1');
         const bar2 = menu.querySelector('.bar2');
         const bar3 = menu.querySelector('.bar3');
@@ -20,3 +20,41 @@ document.addEventListener('DOMContentLoaded', function () {
         bar3.classList.toggle('bar3-animate');
     });
 });
+
+
+//FUNCIONALIDADE DO HEADER
+  let lastScrollTop = 0;
+  const header = document.querySelector('header');
+
+  window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+      
+      header.classList.add('hide');
+    } else {
+      
+      header.classList.remove('hide');
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  });
+
+  //Simular Json
+  document.getElementById("form-contato").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio real
+
+    const dados ={ 
+      nome: document.getElementById("nomeContato").value,
+      email: document.getElementById("emailContato").value,
+      telefone: document.getElementById("telefoneContato").value,
+      assunto: document.getElementById("assuntoContato").value,
+      mensagem: document.getElementById("mensagemContato").value
+    };
+    const jsonSimulado = JSON.stringify(dados, null, 2);
+
+    alert("Dados enviados com sucesso:\n\n" + jsonSimulado);
+
+    this.reset(); // limpa os campos
+  });
+
